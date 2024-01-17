0eee94cdf4a74d5c231b70762f3146c68ae282cd
lovekaur2006 <kaurlove7206@gmail.com>
2024-01-17 11:27:01 +0530

    JS Files

---
+++ operator.js
@@ -0,0 +1,45 @@
+//Operators in JS
+//1. Arithmetic operators
+let a = 3;
+let b = 5;
+console.log("The sum of a and b is " + (a + b));
+console.log("The difference of a and b is " + (a - b));
+console.log("The multiplication of a and b is " + (a * b));
+console.log("The division of a and b is " + (a / b));
+console.log("The modulus of a and b is " + (a % b));
+
+//Assignment operators
+let y = 7;
+// y += 7;
+// y -= 7;
+// y *= 7;
+y /= 7;
+console.log(y);
+
+//comparison operators
+let c = 9;
+let d = 8;
+console.log(a == b);
+console.log(a != b);
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
+
+//Logical operators
+let e = 4;
+let f = 7;
+console.log((e == f) && (e >= f));
+console.log((e == f) || (e >= f));
+console.log(!(e == f));
+
+//Bitwise operator
+let g = 5;
+let h = 1;
+console.log(g & h);
+console.log(g | h);
+console.log(~g);
+console.log(g ^ h);
+console.log(g << h);
+console.log(g >> h);
+
---
+++ varriable.js
@@ -0,0 +1,18 @@
+
+//Declaring variables using 'var':
+var a = 78;
+var b = "love";
+console.log(a);
+console.log(b);
+
+//Declaring variables using 'let':
+let x = 56;
+x = 45;
+console.log(x);
+
+// Declaring variables in JS using 'const':
+//cannot be reassigned
+const pi = 3.14;
+//pi = 2.22;------> This will show an error
+console.log(pi);
+
