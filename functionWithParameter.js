5412b6e2a51943de0f61dab80172e7a1e397f065
Lovepreet Kaur <146243512+lovekaur2006@users.noreply.github.com>
2024-01-19 20:56:53 +0530

    DAY-4

---
+++ functionWithParameter.js
@@ -0,0 +1,13 @@
+//Function with parameter
+function sum(a, b, c) {
+    let d = a + b + c;
+    return d;
+}
+console.log(sum(2, 3, 4));
+
+//--------------------
+function product(x, y) {
+    let z = x * y;
+    return z;
+}
+console.log(product(4, 5));
---
+++ functionWithoutParameter.js
@@ -0,0 +1,18 @@
+//Function without arguements
+function sum() {
+    var i = 5;
+    var j = 4;
+    var k = 6;
+    var l = i + j + k;
+    return l;
+}
+console.log(sum());
+
+//--------------------
+function product() {
+    var x = 3;
+    var y = 8;
+    var z = x * y;
+    return z;
+}
+console.log(product());
