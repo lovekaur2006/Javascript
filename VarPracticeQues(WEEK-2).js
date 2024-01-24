93eb0032ad85137646b0b3bf8211c80fc2e18159
Lovepreet Kaur <146243512+lovekaur2006@users.noreply.github.com>
2024-01-24 20:03:19 +0530

    PRACTICE WEEK-2

---
+++ VarPracticeQues(WEEK-2).js
@@ -0,0 +1,74 @@
+//Store two integers in two variables x and y. Print the sum of two.
+/*
+let x = 4;
+let y = 5;
+let z = x + y;
+console.log("The sum of x and y is " + (z));
+*/
+
+//Store two integers in two variables x and y. Print the product of the two.
+let x = 9;
+let y = 7;
+let z = x * y;
+console.log("The product of x and y is " + (z));
+
+//Calculate the average of 5 subjects
+let Maths = 90;
+let Engl = 89;
+let Hindi = 78;
+let Chem = 79;
+let Phy = 96;
+let avg = (Maths + Engl + Hindi + Chem + Phy) / 5;
+console.log("The average of 5 Subject is : " + avg);
+
+//Calculate Area of Circle, square, rectangle, triangle
+let r = 3;
+let Circle = 3.14 * r * r;
+console.log("The area of circle is : " + Circle);
+
+let a = 2;
+let square = a * a;
+console.log("The area of square is : " + square);
+
+let l = 3;
+let b = 4;
+let h = 5;
+let rectangle = l * b * h;
+let triangle = (b * h) / 2;
+console.log("The arae of triangle is : " + triangle);
+console.log("The area of rectangle is : " + rectangle);
+
+//Calculate simple intrest
+let P = 2000;
+let R = 30;
+let T = 2;
+let SI = (P * R * T) / 100;
+console.log("The simple intrest will be : " + SI);
+
+//Calculate the circumference of the circle
+let radius = 7;
+let circ = 2 * 3.14 * radius;
+console.log("The circumference of the circle is : " + circ);
+
+//Write a program of all the arithmetic operations of Javascript of any two numbers stored in a variable num1 and num2. Also, print the result to the console.
+
+let i = 6;
+let j = 9;
+console.log("The sum of i and j is " + (i + j));               //15
+console.log("The difference of i and j is " + (i - j));        //-3
+console.log("The multiplication of i and j is " + (i * j));    //54
+console.log("The division of i and j is " + (i / j));          //0.6666
+console.log("The modulus of i and j is " + (i % j));           //6
+
+//Write a program to demonstrate the results of comparison operators. Note that both the true and false condition for each operator must be specified.
+
+let c = 4;
+let d = 7;
+console.log(c == d);    //false
+console.log(c != d);    //true
+console.log(c < d);     //true
+console.log(c > d);     //false
+console.log(c <= d);    //true
+console.log(c >= d);    //false
+
+
